bed0a47f1ab72261f22b4ef20dd776aa322a57ac
Lap <lapconghoang@gmail.com>
2024-02-02 00:35:52 +0700

    conrainer

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -4,7 +4,7 @@ const url = "/products";
 export const loader = async ({ Request }) => {
   const response = await customFetch(url);
   const products = response.data.data;
-  const meta = response.data.data;
+  const meta = response.data.meta;
   return { products, meta };
 };
 const Products = () => {
